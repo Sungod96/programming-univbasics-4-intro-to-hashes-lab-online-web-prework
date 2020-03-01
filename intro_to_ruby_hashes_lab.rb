f6b1d8793408caeb789f28a32d4a0e1aedbdcc53
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-03-01 23:16:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,45 +1,44 @@
 def new_hash
-new_hash = {
-  
-}
+new_hash = {}
 end
 
 def my_hash
- my_hash = {
+ my_hash = { 
    :apples => 4,
    :bananas => 5
  }
 end
 
 def pioneer
-  pioneer = {
-    :name => "Grace Hopper"
-  }
+pioneer = {
+  :name => "Grace Hopper"
+}
 end
 
 def id_generator
   id_generator = {
-   :id => 3 
+    :id => 3
   }
 end
 
 def my_hash_creator(key, value)
-  hash = {
-  }
-hash[key] = value
+hash = {
+  
+}
+hash[key] = value 
 return hash
 end
 
 def read_from_hash(hash, key)
-  hash[key] 
+  hash[key]
 
   end 
 
 def update_counting_hash(hash, key)
-    if hash[key]
-      hash[key] += 1
-    else 
-      hash[key] = 1
- end
- return hash
+   if hash[key]
+     hash[key] += 1 
+   else
+     hash[key] = 1 
+   end
+   return hash
  end 
